5c3135c9b154fcafa876680c95cd392e9070ff53
HimanshuBCS <himanshu.pandey@bridgeconn.com>
2017-03-23 15:36:28 +0530

    Deleted useless file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import FetchDemo from './FetchDemo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FetchDemo />
       </div>
     );
   }
