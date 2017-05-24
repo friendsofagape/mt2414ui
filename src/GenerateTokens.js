af6ac91085b6318ead0d43890c80932a3298d561
HimanshuBCS <himanshu.pandey@bridgeconn.com>
2017-05-24 15:50:56 +0530

    Successfully Token generated

--- src/GenerateTokens.js
+++ src/GenerateTokens.js
@@ -14,8 +14,8 @@ import Header from './Header';
 import Footer from './Footer';
 import { FormControl } from 'react-bootstrap';
 import Languages from './Languages';
-import Versions from './version';
 import $ from 'jquery';
+
 
 class GenerateTokens extends Component {
   constructor(props) {
@@ -24,11 +24,14 @@ class GenerateTokens extends Component {
     this.state = {
       language: '',
       version: '',
-      revision: ''
+      revision: '',
+      uploaded:'uploadingStatus',
     }
       // Upload file specific callback handlers
       this.onSelect = this.onSelect.bind(this);
-      this.createSource = this.createSource.bind(this);
+      this.downloadTokenWords = this.downloadTokenWords.bind(this);
+      this.generateConcordance = this.generateConcordance.bind(this);
+      this.JSONToCSVConvertor = this.JSONToCSVConvertor.bind(this);
   }
   
   onSelect(e) {
@@ -36,29 +39,107 @@ class GenerateTokens extends Component {
       [e.target.name]: e.target.value });
   }
 
-  createSource(e){
+// For Downloads Token words
+  downloadTokenWords(e){
     console.log("language: " + this.state.language);
     console.log("version: " + this.state.version);
     console.log("revision: " + this.state.revision);
     e.preventDefault();
     var _this = this
+    var data = { 
+        "language": this.state.language, "version": this.state.version, "revision": this.state.revision 
+      }
     $.ajax({
       url: "http://127.0.0.1:8000/v1/tokenwords",
-      data: {"language": this.state.language, "version": this.state.version, "revision": this.state.revision },
+      contentType: "application/json; charset=utf-8",
+      data : JSON.stringify(data),
       method : "POST",
       headers: {
                 "Authorization": "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzb3VyY2V0ZXh0QHlvcG1haWwuY29tIn0.Xh4Lc8A8Q-l0a6Vjy-KuLK0u6u-et28omajdlPWJY8E"
-
       },
       success: function (result) {
-          _this.setState({uploaded:'success'})
+        if (result){
+         _this.JSONToCSVConvertor(result, "Tokens Report", true);
+        }
+        console.log("Success in Generate Tokens");
+        _this.setState({uploaded:'success'})
       },
       error: function (error) {
-         console.log(error);
+         console.log("Failled in Generate Tokens");
           _this.setState({uploaded:'failure'}) 
       }
     });  
+  }
+
+  JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    var arrData1 = typeof JSONData !== 'object' ? JSON.parse(JSONData) : JSONData;
     
+    var arrData = arrData1.tokenwords
+
+    var CSV = '';       
+    CSV += ReportTitle + '\r\n\n';
+
+    if (ShowLabel) {
+        var row = "";
+          for (var index in arrData[0]) {
+          row += index + ',';
+          console.log("Row: " + arrData);
+        }
+        row = row.slice(0, -1);
+        CSV += row + '\r\n';
+    }
+    
+    for (var i = 0; i < arrData.length; i++) {
+        for (index in arrData[i]) {
+          // row += '"' + arrData[i][index] + '",';    
+        }
+        row.slice(0, row.length - 1);
+        CSV += row + '\r\n';
+    }
+
+    if (CSV === '') {        
+        alert("Invalid data");
+        return;
+    }   
+
+    var fileName = "TokenWords_";
+    fileName += ReportTitle.replace(/ /g,"_");   
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    var link = document.createElement("a");    
+    link.href = uri;
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// For Generating Concordance
+  generateConcordance(e){
+    console.log("language: " + this.state.language);
+    console.log("version: " + this.state.version);
+    e.preventDefault();
+    var _this = this
+    var data = {
+      "language": this.state.language, "version": this.state.version
+    }
+    $.ajax({
+      url: "http://127.0.0.1:8000/v1/generateconcordance",
+      contentType: "application/json; charset=utf-8",
+      data : JSON.stringify(data),
+      method : "POST",
+      headers: {
+                "Authorization": "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzb3VyY2V0ZXh0QHlvcG1haWwuY29tIn0.Xh4Lc8A8Q-l0a6Vjy-KuLK0u6u-et28omajdlPWJY8E"
+      },
+      success: function (result) {
+        console.log("Concordances Created and Stored in DB");
+        _this.setState({uploaded:'success'})
+      },
+      error: function (error) {
+        console.log("Concordances was not generated !!!");
+        _this.setState({uploaded:'failure'}) 
+      }
+    });  
   }
 
   render() {
@@ -67,7 +148,7 @@ class GenerateTokens extends Component {
         <Header/ >
         <div className="col-xs-12 col-md-6 col-md-offset-3">
           <form className="col-md-8 uploader" encType="multipart/form-data">
-            <h1 className="source-header">GenerateToken</h1>&nbsp;
+            <h1 className="source-header">Generate Token</h1>&nbsp;
             <div className={"alert " + this.state.uploaded === 'success'? 'alert-success' : 'invisible'}>
                 <strong>File Uploaded Successfully !!!</strong>
             </div>
@@ -81,10 +162,8 @@ class GenerateTokens extends Component {
                     </FormControl>
               </div>&nbsp;
               <div className="form-group">
-                <lable className="control-label"> <strong> Version </strong> </lable>
-                    <FormControl value={this.state.version} onChange={this.onSelect} name="version" componentClass="select" placeholder="select">
-                      {Versions.map((version, i) => <option  key={i} value={version.code}>{version.value}</option>)}
-                    </FormControl>
+                <lable className="control-lable"> <strong> Version </strong> </lable>
+                    <input value={this.state.version} onChange={this.onSelect} name="version" type="text"  placeholder="version" className="form-control"/> 
               </div>&nbsp;
               <div className="form-group">
                 <lable className="control-lable"> <strong> Revision </strong> </lable>
@@ -92,8 +171,8 @@ class GenerateTokens extends Component {
               </div>&nbsp;
               <div className="form-group">
                 <div className="form-group">
-                  <button id="button" type="button" className="btn btn-success" onClick={this.createSource}>Download</button>&nbsp;&nbsp;
-                  <button id="button" type="button" className="btn btn-success" onClick={this.createSource}>Generating Concordance</button>
+                  <button id="button" type="button" className="btn btn-success" onClick={this.downloadTokenWords}>Download Token</button>&nbsp;&nbsp;
+                  <button id="button" type="button" className="btn btn-success" onClick={this.generateConcordance}>Generating Concordance</button>
                 </div>
               </div>
           </form>
