0fc60875539d472d28fbae19c31826655e8d24ce
HimanshuBCS <himanshu.pandey@bridgeconn.com>
2017-05-31 12:05:25 +0530

    Response added in UI for get languages

--- src/GetLanguages.js
+++ src/GetLanguages.js
@@ -10,13 +10,15 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import $ from 'jquery';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,} from 'material-ui/Table';
 
 class GetLanguages extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      GetLanguages: []
+      getLanguages: [],
+      getVersions: []
     }
       // Upload file specific callback handlers
       this.getLanguages = this.getLanguages.bind(this);
@@ -39,6 +41,13 @@ class GetLanguages extends Component {
       headers: {
                 "Authorization": "bearer " + JSON.stringify(accessToken['access_token']).slice(1,-1)},
       success: function (result) {
+        var getLang = JSON.parse(result);
+        for (var i = 0; i < getLang.length; i++) {
+        var getLang1 = getLang[i];
+        _this.state.getLanguages.push(getLang1[0]+ " ")
+        _this.state.getVersions.push(getLang1[1]+ " ")
+        }
+
        _this.setState({GetLanguages: result})
       },
       error: function (error) {
@@ -55,25 +64,28 @@ class GetLanguages extends Component {
         <div className="col-xs-12 col-md-6 col-md-offset-3">
           <form className="col-md-8 uploader" encType="multipart/form-data">
             <h1 className="source-header">Get Languages</h1>&nbsp;
-                <div className="form-group">
-                  <button id="button" type="button" className="btn btn-success sourcefooter" onClick={this.getLanguages}>Get Languages</button>&nbsp;&nbsp;&nbsp;
-                  </div>
-                  <div className="row">
-                    <div className="col-md-12">
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th className="col-md-4">Languages</th>
-                                    {this.state.JSON.parse(GetLanguages[0])}
-                                    <th >Versions</th>
-                                    {this.state.GetLanguages}
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                  </div>
-             </form>
-          </div>
+              <div className="form-group">
+                <button id="button" type="button" className="btn btn-success sourcefooter" onClick={this.getLanguages}>Get Languages</button>&nbsp;&nbsp;&nbsp;
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <table className="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Languages</th>
+                        <th >Versions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                    <td>{this.state.getLanguages}</td>
+                    <td>{this.state.getVersions}</td>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+          </form>
+        </div>
         <Footer/>
       </div>
       );
