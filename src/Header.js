f0b5cd00441a9d0f779e9d4a331349d3908318b7
himanshup90 <himanshup895@gmail.com>
2017-06-12 15:50:08 +0530

    Added Download Tokens & Concordances place of Generate Tokens

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
         <Navbar inverse collapseOnSelect fixedTop>
         <Navbar.Header><Navbar.Brand>
-            <a href="/signin">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;Autographa MT: Machine Translation Engine</a>
+            <a href="/signin">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;AutographaMT: Machine Translation Engine</a>
           </Navbar.Brand><Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
@@ -15,11 +15,12 @@ class Header extends Component {
             <NavItem eventKey={1} ><Link to={'/signin'}>Sign in</Link></NavItem>
             <NavItem eventKey={2} ><Link to={'/getlanguages'}>Get Languages</Link></NavItem>
             <NavItem eventKey={3} ><Link to={'/uploadsource'}>Upload Source </Link></NavItem>
-            <NavItem eventKey={4} ><Link to={'/generatetokens'}>Generate Tokens</Link> </NavItem>
+            <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
             <NavItem eventKey={5} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
             <NavItem eventKey={6} ><Link to={'/generateconcordance'}>Generate Concordance</Link></NavItem>
             <NavItem eventKey={7} ><Link to={'/getconcordances'}>Get Concordances</Link> </NavItem>
             <NavItem eventKey={8} ><Link to={'/gettranslationdraft'}>Get Translation Draft</Link> </NavItem>
+            <NavItem eventKey={9} ><Link to={'/uitranslation'}>UI</Link></NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import { createStore, applyMiddleware } from 'redux';
 import SignupForm from './SignupForm';
 import SigninForm from './SigninForm';
 import UploadSource from './UploadSource';
-import GenerateTokens from './GenerateTokens';
+import DownloadTokens from './DownloadTokens';
 import GetConcordances from './GetConcordances';
 import GenerateConcordance from './GenerateConcordance';
 import GetTranslationDraft from './GetTranslationDraft';
 import GetLanguages from './GetLanguages';
 import UploadTokens from './UploadTokens';
+import UITranslation from './UITranslation';
 
 const store = createStore(
       (state = {}) => state,
@@ -27,11 +28,12 @@ ReactDOM.render(
       <Route path="/signin" component={SigninForm} />
       <Route path="/getlanguages" component={GetLanguages}/>
       <Route path="/uploadsource" component={UploadSource} />
-      <Route path="/generatetokens" component={GenerateTokens}/>
+      <Route path="/downloadtokens" component={DownloadTokens}/>
       <Route path="/uploadtokens" component={UploadTokens}/>
       <Route path="/getconcordances" component={GetConcordances}/>
       <Route path="/generateconcordance" component={GenerateConcordance}/>
       <Route path="/gettranslationdraft" component={GetTranslationDraft}/>
+      <Route path="/uitranslation" component={UITranslation}/>
     </Router>
   </Provider>,
   document.getElementById('root')
