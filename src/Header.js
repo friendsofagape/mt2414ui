37d77b3deecf4124cc2934342c4f49dea31eb422
himanshup90 <himanshup895@gmail.com>
2017-06-27 09:34:06 +0530

    Entered space between Title name

--- src/Header.js
+++ src/Header.js
@@ -7,12 +7,12 @@ class Header extends Component {
     return (
         <Navbar inverse collapseOnSelect fixedTop>
         <Navbar.Header><Navbar.Brand>
-            <a href="/homepage">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;AutographaMT: Machine Translation Engine</a>
+            <a href="/homepage">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;Autographa MT: Machine Translation Engine</a>
           </Navbar.Brand><Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={2} ><Link to={'/getlanguages'}>Available Source Texts </Link></NavItem>
+            <NavItem eventKey={2} ><Link to={'/getlanguages'}>Available Source Texts</Link></NavItem>
             <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
             <NavItem eventKey={5} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
             <NavItem eventKey={8} ><Link to={'/gettranslationdraft'}>Translation Draft</Link> </NavItem>
