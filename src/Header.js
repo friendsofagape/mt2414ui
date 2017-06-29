c43ac75561605e90439d2428d3a370bfdbe4be4a
himanshup90 <himanshup895@gmail.com>
2017-06-29 15:58:37 +0530

    Changed Download Tokens to Download Wordlist

--- src/Header.js
+++ src/Header.js
@@ -7,15 +7,15 @@ class Header extends Component {
     return (
         <Navbar inverse collapseOnSelect fixedTop>
         <Navbar.Header><Navbar.Brand>
-            <a href="/homepage">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;Autographa MT: Machine Translation Engine</a>
+            <a href="/homepage">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;Autographa MT</a>
           </Navbar.Brand><Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
             <NavItem eventKey={2} ><Link to={'/getlanguages'}>Available Source Texts</Link></NavItem>
-            <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
+            <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Wordlist</Link> </NavItem>
             <NavItem eventKey={5} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
-            <NavItem eventKey={8} ><Link to={'/gettranslationdraft'}>Translation Draft</Link> </NavItem>
+            <NavItem eventKey={8} ><Link to={'/gettranslationdraft'}>Download Translation Draft</Link> </NavItem>
             <NavItem eventKey={1} ><Link to={'/homepage'}>Log out</Link></NavItem>
           </Nav>
         </Navbar.Collapse>
