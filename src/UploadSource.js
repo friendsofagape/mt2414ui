574e73086c172a417630aac1ac3eecdb3ba3f270
himanshup90 <himanshup895@gmail.com>
2017-07-11 11:29:13 +0530

    Added the User Role for Admin

--- src/UploadSource.js
+++ src/UploadSource.js
@@ -10,12 +10,32 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router';
 import Footer from './Footer';
 import { FormControl } from 'react-bootstrap';
 import SourceLanguages from './SourceLanguages';
 import $ from 'jquery';
 import GlobalURL from './GlobalURL';
+
+class Header extends Component {
+  render() {
+    return (
+        <Navbar inverse collapseOnSelect fixedTop >
+        <Navbar.Header><Navbar.Brand>
+            <a href="/homepage">&nbsp;<span className='glyphicon glyphicon-home'></span>&nbsp;&nbsp;Autographa MT</a>
+          </Navbar.Brand><Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse >
+          <Nav className="customHeaderAdmin">
+            <NavItem eventKey={1} ><Link to={'/admin'}>Upload Source</Link></NavItem>
+            <NavItem eventKey={1} ><Link to={'/homepage'}>Log out</Link></NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
 
 class UploadSource extends Component {
   constructor(props) {
@@ -86,7 +106,7 @@ class UploadSource extends Component {
         data : JSON.stringify(data),
         method : "POST",
         headers: {
-                  "Authorization": "bearer " + JSON.stringify(accessToken['access_token']).slice(1,-1)
+                  "Authorization": "bearer " + accessToken
         },
         // eslint-disable-next-line
         success: function (result) {
