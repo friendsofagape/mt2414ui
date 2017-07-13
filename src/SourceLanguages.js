e6af0b854a91e4e33f8db295214b2b6cee90edb5
himanshup90 <himanshup895@gmail.com>
2017-07-13 11:38:57 +0530

    Added new language name Noiri

--- src/SourceLanguages.js
+++ src/SourceLanguages.js
@@ -1,3 +1,3 @@
 export default [
-    {"tam" : "Tamil", "eng" : "English", "amr" : "Assamese", "ben" : "Bengali", "guj" : "Gujarati", "hin" : "Hindi", "mar" : "Marathi", "nep" : "Nepali", "pan" : "Punjabi", "tel" : "Telugu", "mal" : "Malayalam", "urd" : "Urdu", "ori" : "Oriya", "kon" : "Konkani", "kas" : "Kashmiri", "kan" : "Kannada", "khn" : "Khandesi"}
+    {"tam" : "Tamil", "eng" : "English", "amr" : "Assamese", "ben" : "Bengali", "guj" : "Gujarati", "hin" : "Hindi", "mar" : "Marathi", "nep" : "Nepali", "pan" : "Punjabi", "tel" : "Telugu", "mal" : "Malayalam", "urd" : "Urdu", "ori" : "Oriya", "kon" : "Konkani", "kas" : "Kashmiri", "kan" : "Kannada", "khn" : "Khandesi", "noi" : "Noiri"}
 ];
