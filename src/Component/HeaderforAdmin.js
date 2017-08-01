4beee1093355796262031424688a79af7293845a
himanshup90 <himanshup895@gmail.com>
2017-08-01 12:52:46 +0530

    Removed create source from header

--- src/Component/HeaderforAdmin.js
+++ src/Component/HeaderforAdmin.js
@@ -12,13 +12,12 @@ class HeaderforAdmin extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} ><Link to={'/createsource'}>Create Source</Link></NavItem>
-            <NavItem eventKey={2} ><Link to={'/admin'}>Upload Source</Link></NavItem>
-            <NavItem eventKey={3} ><Link to={'/getlanguages'}>Available Texts</Link></NavItem>
-            <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
-            <NavItem eventKey={5} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
-            <NavItem eventKey={6} ><Link to={'/gettranslationdraft'}>Download Draft</Link> </NavItem>
-            <NavItem eventKey={7} ><Link to={'/homepage'}>Log out</Link></NavItem>
+            <NavItem eventKey={1} ><Link to={'/admin'}>Upload Source</Link></NavItem>
+            <NavItem eventKey={2} ><Link to={'/getlanguages'}>Available Texts</Link></NavItem>
+            <NavItem eventKey={3} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
+            <NavItem eventKey={4} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
+            <NavItem eventKey={5} ><Link to={'/gettranslationdraft'}>Download Draft</Link> </NavItem>
+            <NavItem eventKey={6} ><Link to={'/homepage'}>Log out</Link></NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
