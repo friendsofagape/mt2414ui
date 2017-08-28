80d272bc84a1d0f0dc60ca4bce35bfa652421145
himanshup90 <himanshup895@gmail.com>
2017-08-28 17:18:41 +0530

    Added the dynamic target language

--- src/Component/ListTargetLanguage.js
+++ src/Component/ListTargetLanguage.js
@@ -8,18 +8,33 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { FormControl } from 'react-bootstrap';
-import  TargetLanguages from '../TargetLanguages';
 
 class ListTargetLanguage extends Component {
 
   render() {
-    var { Targetlanguage } = this.props;
+    var { Tar } = this.props;
+    var { Language } = this.props;
+
+    var LanguagesWithCode = [];
+    if(Language != null){
+        Object.keys(Language).map(function(data, index){
+        for(var i=0; i<Tar.length;i++){
+          if(Language[data] === Tar[i]){
+            LanguagesWithCode[Tar[i]] = data;
+          }
+        }
+        return (<h1>{null}</h1>);
+      })
+    }
+
     return(
           <FormControl componentClass="select" onChange={this.props.onChange} placeholder="select">
             <option>Choose</option>
-            {Targetlanguage.map(function(data, index){
-              return (<option key={index} value={data}>{TargetLanguages[0][data]}</option>);
-            })}
+            {
+              Object.keys(LanguagesWithCode).map(function(data, index){
+                return(<option key={index} value={data}>{LanguagesWithCode[data]}</option>);
+              })
+            }
           </FormControl>
         );
     }
