fe435b916d7a47d7e4bc7a83b0aea93443e5d262
himanshup90 <himanshup895@gmail.com>
2017-09-08 11:13:47 +0530

    Spaces removed

--- src/Component/Versions.js
+++ src/Component/Versions.js
@@ -10,20 +10,19 @@ import '../App.css';
 import { FormControl } from 'react-bootstrap';
 
 class Versions extends Component {
-
   render() {
     var { version } = this.props;
     return(
-          <FormControl componentClass="select" onChange={this.props.onChange} placeholder="select">
-            <option>Choose</option>
-            {
-              version.map(function(data, index){
-                return (<option key={index} value={data}>{data}</option>);
-              })
-            }
-          </FormControl>
-        );
-    }
+      <FormControl componentClass="select" onChange={this.props.onChange} placeholder="select">
+          <option>Choose</option>
+          {
+            version.map(function(data, index){
+              return (<option key={index} value={data}>{data}</option>);
+            })
+          }
+      </FormControl>
+    );
+  }
 }
 
 export default Versions;
