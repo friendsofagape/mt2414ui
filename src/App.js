5aba79686d929e6956dec206b731cc50d30f6d90
himanshup90 <himanshup895@gmail.com>
2017-09-19 15:16:51 +0530

    Added responsie header for all API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
-import SigninForm from './SigninForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>hello</h1>
+      <div>
         <Header />
         <Footer />
       </div>
--- src/Component/HeaderforSuperAdmin.js
+++ src/Component/HeaderforSuperAdmin.js
@@ -5,20 +5,21 @@ import { Link } from 'react-router';
 class HeaderforSuperAdmin extends Component {
  render() {
    return (
-       <Navbar inverse collapseOnSelect fixedTop>
+       <Navbar inverse>
        <Navbar.Header><Navbar.Brand>
-           <a href="/getlanguages">&nbsp;<span className='glyphicon glyphicon-home'></span></a>
+           <a href="/getlanguages"><span className='glyphicon glyphicon-home scolor'></span></a>
        </Navbar.Brand><Navbar.Toggle />
        </Navbar.Header>
        <Navbar.Collapse>
-         <Nav>
-           <NavItem eventKey={1} ><Link to={'/superadmin'}>Super Admin</Link></NavItem>
-           <NavItem eventKey={2} ><Link to={'/admin'}>Upload Source</Link></NavItem>
-           <NavItem eventKey={3} ><Link to={'/getlanguages'}>Available Texts</Link></NavItem>
-           <NavItem eventKey={4} ><Link to={'/downloadtokens'}>Download Tokens</Link> </NavItem>
-           <NavItem eventKey={5} ><Link to={'/uploadtokens'}>Upload Tokens</Link> </NavItem>
-           <NavItem eventKey={6} ><Link to={'/gettranslationdraft'}>Download Draft</Link> </NavItem>
-           <NavItem eventKey={7} ><Link to={'/logout'}>Log out</Link></NavItem>
+         <Nav className="pull-right">
+           <NavItem eventKey={1} ><Link to={'/superadmin'}><strong className="scolor">Super Admin </strong></Link></NavItem>
+           <NavItem eventKey={2} ><Link to={'/admin'}><strong className="scolor">Upload Source </strong></Link></NavItem>
+           <NavItem eventKey={3} ><Link to={'/getlanguages'}><strong className="scolor">Available Texts</strong></Link></NavItem>
+           <NavItem eventKey={4} ><Link to={'/downloadtokens'}><strong className="scolor">Download Tokens </strong></Link> </NavItem>
+           <NavItem eventKey={5} ><Link to={'/uploadtokens'}><strong className="scolor">Upload Tokens </strong></Link> </NavItem>
+           <NavItem eventKey={6} ><Link to={'/gettranslationdraft'}><strong className="scolor">Download Draft </strong></Link> </NavItem>
+           <NavItem eventKey={7} ><Link to={'/translation'}><strong className="scolor">Translation </strong></Link></NavItem>
+           <NavItem eventKey={8} ><Link to={'/logout'}><strong className="scolor">Log out </strong></Link></NavItem>
          </Nav>
        </Navbar.Collapse>
      </Navbar>
