f4ba7bbe2f2b5d7efcd38d65f0d578652cc59fd9
himanshup90 <himanshup895@gmail.com>
2017-09-19 15:21:15 +0530

    Added footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 class Footer extends Component{
  render(){
    return (
-         <div className="footer navbar-fixed-bottom">
-          <a href="#"></a>
+         <div className="footer top1 inverse">
+          <a href="#"><strong className="scolor">Contact us: autographamt@gmail.com</strong></a>
          </div>
     );
  }
