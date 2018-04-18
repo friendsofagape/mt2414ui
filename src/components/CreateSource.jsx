cbc4394e855ef6cc5dba00ab2044e48649733ba0
HimanshuBCS <himanshu.pandey@bridgeconn.com>
2018-04-18 15:55:52 +0530

    create source issue fixed

--- src/components/CreateSource.jsx
+++ src/components/CreateSource.jsx
@@ -1,7 +1,7 @@
 /**
  * @module src/CreateSource
  *
- * Component that display SourceDocument
+ * Component that create source for upload source
  * Accepts the following properties:
  *  - language: Ethnologue code of the language
  *  - version: version of source language
@@ -12,10 +12,10 @@ import Footer from './Footer';
 import $ from 'jquery';
 import GlobalURL from './GlobalURL';
 import Header from './Header';
-import VirtualizedSelect from 'react-virtualized-select'
-import 'react-select/dist/react-select.css'
-import 'react-virtualized/styles.css'
-import 'react-virtualized-select/styles.css'
+import VirtualizedSelect from 'react-virtualized-select';
+import 'react-select/dist/react-select.css';
+import 'react-virtualized/styles.css';
+import 'react-virtualized-select/styles.css';
 
 class CreateSource extends Component {
   constructor(props) {
@@ -36,18 +36,18 @@ class CreateSource extends Component {
     this.updateLanguageList = this.updateLanguageList.bind(this);
   }
 
-  componentDidMount() {
-      var _this = this;
-      let accessToken = JSON.parse(window.localStorage.getItem('access_token')) 
-      $.ajax({
+  componentWillMount() {
+    let _this = this;
+    let accessToken = JSON.parse(window.localStorage.getItem('access_token')) 
+    $.ajax({
       url: GlobalURL["hostURL"]+"/v1/languagelist",
       contentType: "application/json; charset=utf-8",
       method : "GET",
       headers: {
-                "Authorization": "bearer " + accessToken
+        "Authorization": "bearer " + accessToken
       },
       success: function (result) {
-        var getTargetLang = JSON.parse(result);
+        let getTargetLang = JSON.parse(result);
         _this.setState({getTargetLangJson: getTargetLang});
       },
       error: function (error) {
@@ -57,60 +57,61 @@ class CreateSource extends Component {
 
   onSelect(e) {
     this.setState({
-      [e.target.name]: e.target.value });
+      [e.target.name]: e.target.value 
+    });
   }
 
   createSource(e){
-    var _this = this
-      var data = { 
-        "language": this.state.selectValue.value, "version": this.state.version
-      }
-      let accessToken = JSON.parse(window.localStorage.getItem('access_token'));
-      $.ajax({
-        url: GlobalURL["hostURL"]+"/v1/createsources",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify(data),
-        method : "POST",
-        async: false,
-        headers: {
-                  "Authorization": "bearer " + accessToken
-        },
-      beforeSend: function () {
-          $(".modal").show();
+    let _this = this;
+    let data = { 
+      "language": this.state.selectValue.value, "version": this.state.version
+    };
+    let accessToken = JSON.parse(window.localStorage.getItem('access_token'));
+    $.ajax({
+      url: GlobalURL["hostURL"]+"/v1/createsources",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+      method: "POST",
+      async: false,
+      headers: {
+        "Authorization": "bearer " + accessToken
       },
-        // eslint-disable-next-line
-        success: function (result) {
+      beforeSend: function () {
+        $(".modal").show();
+      },
+      // eslint-disable-next-line
+      success: function (result) {
         result = JSON.parse(result)
-          if (result.success !== false) {
-          _this.setState({message: result.message, uploaded: 'success'})
+        if (result.success !== false) {
+          _this.setState({message: result.message, uploaded: 'success'});
           setTimeout(function(){
             window.location.href = "./admin";
-          },2000)
-        }else {
-          _this.setState({message: result.message, uploaded: 'failure'})
+          }, 2000);
+        } else {
+          _this.setState({message: result.message, uploaded: 'failure'});
           setTimeout(function(){
-            _this.setState({uploaded: 'fail'})
-          },5000);
-          }
+            _this.setState({uploaded: 'fail'});
+          }, 5000);
+        }
       },
       error: function (error) {
-        _this.setState({uploaded: 'failure'})
+        _this.setState({uploaded: 'failure'});
         setTimeout(function(){
-          _this.setState({uploaded: 'fail'})
-        },5000);
+          _this.setState({uploaded: 'fail'});
+        }, 5000);
       }
-      });
-    }
+    });
+  }
 
   updateLanguageList(e){
-    var _this = this;
-    let accessToken = JSON.parse(window.localStorage.getItem('access_token')) 
+    let _this = this;
+    let accessToken = JSON.parse(window.localStorage.getItem('access_token'));
     $.ajax({
       url: GlobalURL["hostURL"]+"/v1/updatelanguagelist",
       contentType: "application/json; charset=utf-8",
-      method : "GET",
+      method: "GET",
       headers: {
-                "Authorization": "bearer " + accessToken
+        "Authorization": "bearer " + accessToken
       },
       beforeSend: function () {
         $(".modal").show();
@@ -120,20 +121,20 @@ class CreateSource extends Component {
       },
       success: function (result) {
         $.ajax({
-        url: GlobalURL["hostURL"]+"/v1/languagelist",
-        contentType: "application/json; charset=utf-8",
-        method : "GET",
-        headers: {
-                  "Authorization": "bearer " + accessToken
-        },
-        success: function (result) {
-          var getTargetLang = JSON.parse(result);
-          _this.setState({getTargetLanguages: getTargetLang})
-        },
-        error: function (error) {
-        }
+          url: GlobalURL["hostURL"]+"/v1/languagelist",
+          contentType: "application/json; charset=utf-8",
+          method : "GET",
+          headers: {
+            "Authorization": "bearer " + accessToken
+          },
+          success: function (result) {
+            let getTargetLang = JSON.parse(result);
+            _this.setState({getTargetLanguages: getTargetLang})
+          },
+          error: function (error) {
+          }
         });
-          var resultMes = JSON.parse(result);
+        let resultMes = JSON.parse(result);
         _this.setState({message: resultMes.message, uploaded: 'success'})
         setTimeout(function(){
           _this.setState({ uploaded: 'fail'})
@@ -145,13 +146,14 @@ class CreateSource extends Component {
   }
 
   render() {
-    
-    var myTarget = this.state.getTargetLangJson;
-    var options = {};
-    var myOptions = [];
-
-    Object.keys(myTarget).map(function(data, index){
-      options = {label: data, value: myTarget[data]};
+    const { getTargetLangJson } = this.state;
+    let options = {};
+    let myOptions = [];
+    Object.keys(getTargetLangJson).map((data, index) => {
+      options = {
+        label: data,
+        value: getTargetLangJson[data]
+      };
       myOptions.push(options);
       return null;
     });
@@ -167,7 +169,6 @@ class CreateSource extends Component {
             <h3 className="top3">Create Sources</h3>
           </div>
         </div>
-
         <div className="row">
             <form className="col-md-12 col-md-4 col-md-offset-4 bodyColor bodyBorder" id="upload_form" encType="multipart/form-data">
             <div className={"alert " + (this.state.uploaded === 'success'? 'alert-success' : 'invisible')}>
@@ -223,8 +224,8 @@ class CreateSource extends Component {
         <Footer/>
       </div>
       </div>
-      );
-    }
+    );
+  }
 }
 
 export default CreateSource;
