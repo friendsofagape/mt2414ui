4e7c7f62b8448c66af22a7e04a16cb6925852d8a
Praveen Bhadri <praveen.bhadri@gmail.com>
2018-07-02 17:10:36 +0530

    changes api url to the staging one

--- src/app/globalUrl.ts
+++ src/app/globalUrl.ts
@@ -5,25 +5,25 @@ export class GlobalUrl {
     
     //In case of get request this api Returns list of positional pairs, list of Hindi words, list of strong numbers for the bcv queried. 
     // and in case of post it updates the corresponding Verses alignments as per the corrections made by user.
-    getnUpdateBCV : string = 'http://127.0.0.1:8000/v2/alignments';
+    // getnUpdateBCV : string = 'http://127.0.0.1:8000/v2/alignments';
 
-    // Returns a list of books whose alignments are available
-    getBooks : string = 'http://127.0.0.1:8000/v2/alignments/books';
+    // // Returns a list of books whose alignments are available
+    // getBooks : string = 'http://127.0.0.1:8000/v2/alignments/books';
 
-    //Returns a list of chapter number of the book queried.
-    getChapters : string = 'http://127.0.0.1:8000/v2/alignments/chapternumbers/';
+    // //Returns a list of chapter number of the book queried.
+    // getChapters : string = 'http://127.0.0.1:8000/v2/alignments/chapternumbers/';
 
-    //Returns a list containing the verse numbers for the particular chapter number of a book.
-    getVerses : string = 'http://127.0.0.1:8000/v2/alignments/versenumbers/';   
+    // //Returns a list containing the verse numbers for the particular chapter number of a book.
+    // getVerses : string = 'http://127.0.0.1:8000/v2/alignments/versenumbers/';   
 
-    // Returns the lexicon data for the corresponding greek strong number
-    getLexicon : string = 'http://127.0.0.1:8000/v2/lexicons';
+    // // Returns the lexicon data for the corresponding greek strong number
+    // getLexicon : string = 'http://127.0.0.1:8000/v2/lexicons';
 
-    // Saves the new approved alignments in the feedback lookup table for further use
-    approveAlignments : string = 'http://127.0.0.1:8000/v2/alignments/feedbacks';
+    // // Saves the new approved alignments in the feedback lookup table for further use
+    // approveAlignments : string = 'http://127.0.0.1:8000/v2/alignments/feedbacks';
 
-    // Saves the new approved alignments in the feedback lookup table for further use
-    fixAlignments : string = 'http://127.0.0.1:8000/v2/alignments/feedbacks/verses';
+    // // Saves the new approved alignments in the feedback lookup table for further use
+    // fixAlignments : string = 'http://127.0.0.1:8000/v2/alignments/feedbacks/verses';
     
     
 
@@ -31,23 +31,23 @@ export class GlobalUrl {
     // For Production Environment
     //In case of get request this api Returns list of positional pairs, list of Hindi words, list of strong numbers for the bcv queried. 
     // and in case of post it updates the corresponding Verses alignments as per the corrections made by user.
-    // getnUpdateBCV : string = 'https://stagingapi.autographamt.com/v2/alignments';
+    getnUpdateBCV : string = 'https://stagingapi.autographamt.com/v2/alignments';
 
-    // // Returns a list of books whose alignments are available
-    // getBooks : string = 'https://stagingapi.autographamt.com/v2/alignments/books';
+    // Returns a list of books whose alignments are available
+    getBooks : string = 'https://stagingapi.autographamt.com/v2/alignments/books';
 
-    // //Returns a list of chapter number of the book queried.
-    // getChapters : string = 'https://stagingapi.autographamt.com/v2/alignments/chapternumbers/';
+    //Returns a list of chapter number of the book queried.
+    getChapters : string = 'https://stagingapi.autographamt.com/v2/alignments/chapternumbers/';
 
-    // //Returns a list containing the verse numbers for the particular chapter number of a book.
-    // getVerses : string = 'https://stagingapi.autographamt.com/v2/alignments/versenumbers/';   
+    //Returns a list containing the verse numbers for the particular chapter number of a book.
+    getVerses : string = 'https://stagingapi.autographamt.com/v2/alignments/versenumbers/';   
 
-    // //Returns the lexicon data for the corresponding greek strong number
-    // getLexicon : string = 'https://stagingapi.autographamt.com/v2/lexicons';
+    //Returns the lexicon data for the corresponding greek strong number
+    getLexicon : string = 'https://stagingapi.autographamt.com/v2/lexicons';
 
-    // Saves the new approved alignments in the feedback lookup table for further use
-    //approveAlignments : string = 'https://stagingapi.autographamt.com/v2/alignments/feedbacks';
+    //Saves the new approved alignments in the feedback lookup table for further use
+    approveAlignments : string = 'https://stagingapi.autographamt.com/v2/alignments/feedbacks';
 
-    // Saves the new approved alignments in the feedback lookup table for further use
-    //fixAlignments : string = 'http://127.0.0.1:8000/v2/alignments/feedbacks/verses';
+    //Saves the new approved alignments in the feedback lookup table for further use
+    fixAlignments : string = 'https://stagingapi.autographamt.com/v2/alignments/feedbacks/verses';
 }
